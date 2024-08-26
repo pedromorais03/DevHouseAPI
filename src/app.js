2b79031cdee19c1517b11c3489a212f49ae6bea4
pedromorais03 <pedromorais3012@gmail.com>
2024-08-26 09:32:27 -0300

    Added mongoose and setup to use mongodb

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import routes from './routes'
 
 class App{
 
   constructor(){
     this.server = express()
+
+    mongoose.connect(`mongodb+srv://devhouse:${process.env.MONGO_DB_PASSWORD}@devhouse.golmv.mongodb.net/devhouse?retryWrites=true&w=majority&appName=DevHouse`)
+
     this.middlewares()
     this.routes()
   }
