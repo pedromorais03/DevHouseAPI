b7e4952f41e8c96bcfe198ef740ab64590361b35
pedromorais03 <pedromorais3012@gmail.com>
2024-08-26 22:51:22 -0300

    Working with multer and saving file from form-data in our directory

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express'
+import multer from 'multer'
+import uploadConfig from './config/upload'
 import SessionController from './controller/SessionController'
 import HouseController from './controller/HouseController'
 
 const routes = new Router()
+const upload = multer(uploadConfig)
 
 routes.post('/sessions', SessionController.store)
-routes.post('/houses', HouseController.store)
+routes.post('/houses', upload.single('thumbnail'), HouseController.store)
 
 export default routes
